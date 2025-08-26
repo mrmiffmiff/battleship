9b2523f23358d8b8cc3d69762cd473690204b4f8
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-08-26 13:08:56 -0700

    fixing some minor syntactical bugs

--- src/controllers/UIController.js
+++ src/controllers/UIController.js
@@ -3,8 +3,8 @@ import { ROWS, COLUMNS } from "../gameboard/gameboard";
 export default class UIController {
     constructor(gameController) {
         this.game = gameController;
-        this.leftGrid = document.querySelector("#board1 board");
-        this.rightGrid = document.querySelector("#board2 board");
+        this.leftGrid = document.querySelector("#board1 .board");
+        this.rightGrid = document.querySelector("#board2 .board");
         this.status = document.querySelector("#status");
     }
 
@@ -12,7 +12,7 @@ export default class UIController {
     #buildBoards() {
         [this.leftGrid, this.rightGrid].forEach((grid) => {
             for (let r = 0; r < ROWS; r++) {
-                for (let c = 0; r < COLUMNS; c++) {
+                for (let c = 0; c < COLUMNS; c++) {
                     const cell = document.createElement("div");
                     cell.classList.add("gridCell");
                     cell.dataset.row = r;
